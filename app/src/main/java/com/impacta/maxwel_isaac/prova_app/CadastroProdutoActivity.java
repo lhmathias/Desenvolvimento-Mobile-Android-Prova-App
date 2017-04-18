ef6a6fea02989db56708a9c8b366bee93001d3e5
isaac.melo <isaac.melo@voxage.com.br>
2017-04-18 11:11:29 -0300

    Add onActivityResult and startActivityForResult.

--- app/src/main/java/com/impacta/maxwel_isaac/prova_app/CadastroProdutoActivity.java
+++ app/src/main/java/com/impacta/maxwel_isaac/prova_app/CadastroProdutoActivity.java
@@ -5,26 +5,62 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 /**
  * Created by maxwel on 16/04/2017.
  */
 
 public class CadastroProdutoActivity extends AppCompatActivity {
+
+    private String nome;
+    private String valor;
+    private String categoria;
+    private boolean favorito;
+
+    private EditText nomeProduto;
+    private EditText valorProduto;
+    private EditText categoriaProduto;
+    private CheckBox favoritoCheckBox;
+    private Button botaoSalvarProduto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_produto);
 
-        Button botaoSalvarProduto = (Button)findViewById(R.id.botaoSalvarProduto);
+        nomeProduto         = (EditText) findViewById(R.id.nomeProduto);
+        valorProduto        = (EditText) findViewById(R.id.ValorProduto);
+        categoriaProduto    = (EditText) findViewById(R.id.categoriaProduto);
+        favoritoCheckBox    = (CheckBox) findViewById(R.id.favoritoCheckBox);
+        botaoSalvarProduto  = (Button)findViewById(R.id.botaoSalvarProduto);
+
         botaoSalvarProduto.setOnClickListener(onClickSalvarProduto());
     }
 
     private View.OnClickListener onClickSalvarProduto(){
         return new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(CadastroProdutoActivity.this, TelaInicialActivity.class);
-                startActivity(intent);
+
+                nome        = nomeProduto.getText().toString();
+                valor       = valorProduto.getText().toString();
+                categoria   = categoriaProduto.getText().toString();
+
+                if (favoritoCheckBox.isChecked()) {
+                    favorito = true;
+                } else {
+                    favorito = false;
+                }
+
+                Intent devolve = new Intent();
+                devolve.putExtra("nomeProduto",nome);
+                devolve.putExtra("valorProduto",valor);
+                devolve.putExtra("categoriaProduto",categoria);
+                devolve.putExtra("favoritoCheckBox",favorito);
+
+                setResult(RESULT_OK, devolve);
+                finish();
             }
         };
     }
--- app/src/main/java/com/impacta/maxwel_isaac/prova_app/TelaInicialActivity.java
+++ app/src/main/java/com/impacta/maxwel_isaac/prova_app/TelaInicialActivity.java
@@ -4,33 +4,77 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.R.drawable;
 
-import java.util.List;
 /**
  * Created by maxwe on 16/04/2017.
  */
 public class TelaInicialActivity extends AppCompatActivity {
+    private TextView nomeProduto;
+    private TextView valorProduto;
+    private TextView categoriaProduto;
+    private ImageView imagemFavorito;
+    private Button botaoCadastroProduto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_inicial);
 
-        Button botaoCadastroProduto = (Button)findViewById(R.id.botaoCadasto);
+        nomeProduto         = (TextView) findViewById(R.id.nomeProduto);
+        valorProduto        = (TextView) findViewById(R.id.valorProduto);
+        categoriaProduto    = (TextView) findViewById(R.id.CategoriaProduto);
+        imagemFavorito  = (ImageView) findViewById(R.id.imagemFavorito);
+
+        botaoCadastroProduto = (Button)findViewById(R.id.botaoCadasto);
         botaoCadastroProduto.setOnClickListener(onClickCadastro());
     }
 
     private View.OnClickListener onClickCadastro() {
         return new View.OnClickListener() {
             public void onClick(View v) {
+
+                nomeProduto.setText("");
+                valorProduto.setText("");
+                categoriaProduto.setText("");
+
+                nomeProduto.setVisibility(View.INVISIBLE);
+                valorProduto.setVisibility(View.INVISIBLE);
+                categoriaProduto.setVisibility(View.INVISIBLE);
+                imagemFavorito.setVisibility(View.INVISIBLE);
+
                 Intent intent = new Intent(TelaInicialActivity.this, CadastroProdutoActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent,1);
             }
         };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode == -1){
+            String nome  = data.getStringExtra("nomeProduto");
+            String valor     = data.getStringExtra("valorProduto");
+            String categoria =  data.getStringExtra("categoriaProduto");
+            Boolean favorito =  data.getExtras().getBoolean("favoritoCheckBox");
+
+            nomeProduto.setVisibility(View.VISIBLE);
+            valorProduto.setVisibility(View.VISIBLE);
+            categoriaProduto.setVisibility(View.VISIBLE);
+            imagemFavorito.setVisibility(View.VISIBLE);
+
+            nomeProduto.setText(nome);
+            valorProduto.setText(valor);
+            categoriaProduto.setText(categoria);
+
+            if(favorito){
+                imagemFavorito.setImageResource(drawable.btn_star_big_on);
+            }else{
+                imagemFavorito.setImageResource(drawable.btn_star_big_off);
+            }
+        }
     }
 }
